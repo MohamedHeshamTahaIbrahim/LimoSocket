2535069e6f0e579285aa2168ab903ce4e502912f
محمد <Mohamed.Hesham_taha20110377@yahoo.com>
2016-03-16 12:19:51 +0200

    Initial commit

---
+++ app/src/main/java/Network/Constants.java
@@ -0,0 +1,8 @@
+package Network;
+
+/**
+ * Created by محمد on 15/03/2016.
+ */
+public class Constants {
+    public static final String SERVER_URL ="http://dev.rytalo.com/limo";
+}
---
+++ app/src/main/java/Network/SocketCommunication.java
@@ -0,0 +1,32 @@
+package Network;
+
+import android.app.Application;
+
+import java.net.URISyntaxException;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+
+/**
+ * Created by محمد on 15/03/2016.
+ */
+public class SocketCommunication extends Application {
+private Socket socket ;{
+
+        try {
+            socket = IO.socket(Constants.SERVER_URL);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public  void socketConnect(){
+        socket.connect();
+    }
+    public void socketDisconnect(){
+        socket.disconnect();
+    }
+
+    public io.socket.client.Socket getSocket() {
+        return socket;
+    }
+}
